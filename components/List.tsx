d478b1857753e5111df1866d33f0ecce0cd3e4dd
Tim Benniks <tbenniks@gmail.com>
2024-02-19 16:40:04 +0100

    simplief the implementation

--- components/List.tsx
+++ components/List.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import Post from "./Post";
-import { getData } from "../helpers";
 
 type Post = {
   cursor: string;
@@ -14,6 +13,56 @@ type Post = {
     };
   };
 };
+
+async function getData(amount = 3, cursor: string) {
+  const results = await fetch(
+    "https://us-east-1-shared-usea1-02.cdn.hygraph.com/content/clcrreocx0oot01ur229906i3/master",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      next: { revalidate: 10 },
+      body: JSON.stringify({
+        query: `
+          query Posts($cursor: String, $amount: Int!) {
+            pages: postsConnection(after: $cursor, first: $amount, orderBy: createdAt_DESC) {
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
+              posts: edges {
+                cursor
+                post: node {
+                  slug
+                  id
+                  createdAt
+                  content {
+                    html
+                  }
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          amount,
+          cursor: cursor || null,
+        },
+      }),
+    }
+  );
+
+  const json = await results.json();
+  const { pages } = json.data;
+  const { endCursor, hasNextPage } = pages.pageInfo;
+
+  return {
+    posts: pages.posts,
+    endCursor,
+    hasNextPage,
+  };
+}
 
 export default function List() {
   const [posts, setPosts] = useState<Post[]>([]);
